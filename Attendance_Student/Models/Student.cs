b3018ad7465a124796efb3021f81c0f0109f9d16
Omar Sayed <151388740+OmarSayed0@users.noreply.github.com>
2024-11-25 23:47:09 +0200

    Update Student Model

---
+++ Attendance_Student/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace SchoolManagement.Models
+{
+    public class Student
+    {
+        public string Status { get; set; }
+        public int ClassId { get; set; }
+        public int ParentId { get; set; }
+        public int AttendanceId { get; set; }
+    }
+}
