8b230fd70c64c268bea3c96f0eebe5d634a10ad4
Omar Sayed <151388740+OmarSayed0@users.noreply.github.com>
2024-11-25 23:17:32 +0200

    Update Connection String

--- Attendance_Student/obj/Debug/net8.0/Attendance_Student.AssemblyInfo.cs
+++ Attendance_Student/obj/Debug/net8.0/Attendance_Student.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Attendance_Student")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28df045b5f625a8ebdbf87c43ed97db1f0246f72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ed71cb82940c7bc1f5f05b10f0a7ca3fb6ddf5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Attendance_Student")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Attendance_Student")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
