c08583cbc04b15816c9fbde628f36c0281c12f63
MohamedSalah10010 <abosalah100001000@gmail.com>
2024-11-27 02:08:16 +0200

    Adding ClassController and GenericRepo in addition to some doc

---
+++ Attendance_Student/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Attendance_Student.Models;
+
+namespace Attendance_Student.Repositories
+{
+    public class GenericRepository<genericEntity> where genericEntity : class
+    {
+        protected AttendanceStudentContext db;
+
+        public GenericRepository(AttendanceStudentContext db)
+        {
+                this.db = db;
+        }
+
+        public virtual List<genericEntity> selectAll()
+        {
+            return db.Set<genericEntity>().ToList();
+        }
+
+        public virtual genericEntity selectById(int id)
+        {
+            return db.Set<genericEntity>().Find(id);
+        }
+
+        public virtual void add(genericEntity entity)
+        {
+            db.Set<genericEntity>().Add(entity);
+            
+        }
+
+        public virtual void update(genericEntity entity)
+        {
+
+            db.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+             
+        }
+        public virtual void remove(int id)
+        {
+
+            var obj = db.Set<genericEntity>().Find(id);
+            db.Set<genericEntity>().Remove(obj);
+
+        }
+
+        public virtual void save()
+        {
+
+            db.SaveChanges();
+        }
+    }
+}
