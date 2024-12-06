b46753724849e3f20ea655a774c30457c2d550d4
ahmed salah <81277940+Ahmedsalahmohammed22@users.noreply.github.com>
2024-12-06 11:44:59 +0200

    Add Attendance and notification and make reports

--- Attendance_Student/obj/Debug/net8.0/Attendance_Student.AssemblyInfo.cs
+++ Attendance_Student/obj/Debug/net8.0/Attendance_Student.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Attendance_Student")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc85de66c6cf6ea789198955249eb05a1cf88270")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b820f3d998bbe9d8c2792032d9826b50940558")]
 [assembly: System.Reflection.AssemblyProductAttribute("Attendance_Student")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Attendance_Student")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
